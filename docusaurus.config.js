6053d094fbfae4d849c718b849478e6b3df5b6a3
Onat <53895969+onattech@users.noreply.github.com>
2023-05-30 13:05:42 +0300

    clean up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url: "https://.pages.dev",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/cloudflare/",
+    baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
